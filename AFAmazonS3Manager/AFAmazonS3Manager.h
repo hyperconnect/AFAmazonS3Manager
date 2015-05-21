e1ac152e4099a98eed95947f69f9b4030643fd6a
Hoon H <hh@hpcnt.com>
2015-05-21 12:10:32 +0900

    Patched to use correct framework import directive.

--- AFAmazonS3Manager/AFAmazonS3Manager.h
+++ AFAmazonS3Manager/AFAmazonS3Manager.h
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "AFHTTPRequestOperationManager.h"
+#import <AFNetworking/AFNetworking.h>
 #import "AFAmazonS3RequestSerializer.h"
 
 /**
